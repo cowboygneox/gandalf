5907d724eeb3337b13b2ecf3ab8ffb1c7a440b74
Sean Freitag <sfreitag@conserviscorp.com>
2016-11-10 12:09:01 -0600

    Improving the documentation and configuration of websockets.

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
-REST = 1
+HTTP = 1
 WEBSOCKET = 2
 
 class GandalfConfiguration:
-    def __init__(self, proxy_host, db_adapter, allowed_hosts, signing_secret='', mode=REST):
+    def __init__(self, proxy_host, db_adapter, allowed_hosts, signing_secret='', mode=HTTP):
         self.proxy_host = proxy_host
         self.db_adapter = db_adapter
         self.allowed_hosts = allowed_hosts
